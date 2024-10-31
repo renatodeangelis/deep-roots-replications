cf2f9a8c6fd8fe38b674bc625a24476d20b3689a
Renato de Angelis <renatodeangelis12@gmail.com>
2024-10-31 14:43:11 -0500

    merges

--- code/8-ajr-2002/ajr-2002-script.R
+++ code/8-ajr-2002/ajr-2002-script.R
@@ -11,3 +11,12 @@ wb_regions = read_csv("datasets/world-regions-according-to-the-world-bank.csv") 
   rename(wb_region = world_region_according_to_the_world_bank)
 malaria = read_dta("datasets/6-nunn/country.dta") |>
   select(code, malfal)
+
+colony_final = colony_data |>
+  left_join(malaria, by = c("shortnam" = "code")) |>
+  left_join(wb_regions, by = c("shortnam" = "code")) #|>
+  mutate(wb_region = case_when(
+    shortnam %in% c("ROM", "YUG") ~ "Europe and Central Asia",
+    shortnam == "TWN" ~ "East Asia and Pacific",
+    shortnam == "ZAR" ~ "Sub-Saharan Africa",
+    TRUE ~ wb_region))
