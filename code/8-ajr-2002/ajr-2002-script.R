cb2dc790bad704f10715184158ddbd9dfc4db38b
Renato de Angelis <renatodeangelis12@gmail.com>
2024-10-31 14:20:57 -0500

    load in data

---
+++ code/8-ajr-2002/ajr-2002-script.R
@@ -0,0 +1,13 @@
+packages = c("dplyr", "readr", "haven", "estimatr", 
+             "conleyreg", "lmtest", "sandwich", "tibble",
+             "quantreg", "stringr", "spatInfer", "modelsummary",
+             "ggplot2", "tinytable", "plm", "stargazer")
+lapply(packages, library, character.only = TRUE)
+
+colony_data = read_dta("datasets/8-ajr-2002/maketable3.dta")
+wb_regions = read_csv("datasets/world-regions-according-to-the-world-bank.csv") |> 
+  janitor::clean_names() |>
+  select(-entity, -year) |>
+  rename(wb_region = world_region_according_to_the_world_bank)
+malaria = read_dta("datasets/6-nunn/country.dta") |>
+  select(code, malfal)
