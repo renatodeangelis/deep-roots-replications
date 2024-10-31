250a7d9a31beb6876b52d4efd9d8ffa4b0e44053
Renato de Angelis <renatodeangelis12@gmail.com>
2024-10-31 11:10:47 -0500

    Added stargazer output

--- code/6-nunn/slave-trade-script.R
+++ code/6-nunn/slave-trade-script.R
@@ -1,7 +1,7 @@
 packages = c("dplyr", "readr", "haven", "estimatr", 
              "conleyreg", "lmtest", "sandwich", "tibble",
              "quantreg", "stringr", "spatInfer", "modelsummary",
-             "ggplot2", "tinytable", "plm")
+             "ggplot2", "tinytable", "plm", "stargazer")
 lapply(packages, library, character.only = TRUE)
 
 slave_data = read_dta("../../datasets/6-nunn/slave_trade_QJE.dta")
@@ -37,3 +37,34 @@ mod4 = lm(ln_maddison_pcgdp2000 ~ ln_export_area + colony0 + colony1 + colony2 +
             islam + legor_fr + region_n + ln_avg_gold_pop + ln_avg_oil_pop +
             ln_avg_all_diamonds_pop + congo_dummy + malaria_dummy,
           data = slave_final)
+
+output = stargazer(
+  mod1, mod2, mod3, mod4,
+  type = "latex",
+  title = "Regression Results",
+  covariate.labels = c(
+    "ln(Exports/Area)",
+    "Malaria Dummy",
+    "Absolute Latitude",
+    "Longitude",
+    "Min Rainfall",
+    "Max Humidity",
+    "Low Temperature",
+    "ln(Coastline/Area)",
+    "Island Dummy",
+    "Islam Dummy",
+    "French Legal Origin",
+    "Region (North)",
+    "ln(Gold/Pop)",
+    "ln(Oil/Pop)",
+    "ln(Diamonds/Pop)",
+    "Congo Dummy"
+  ),
+  add.lines = list(c("Colonizer Fixed Effects", "\\checkmark", "\\checkmark", "\\checkmark", "\\checkmark")),
+  omit = "colony",
+  no.space = TRUE,
+  column.labels = c("(1)", "(2)", "(3)", "(4)"),
+  dep.var.labels = "ln(GDP per Capita)",
+  star.cutoffs = c(0.05, 0.01, 0.001)
+)
+
