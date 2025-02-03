a479f4a1aca42b2c8ffb0cda37a096174073131e
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-03 13:48:24 -0600

    added PWT to AJR 2001

--- code/7-ajr-2001/ajr-2001-script.R
+++ code/7-ajr-2001/ajr-2001-script.R
@@ -1,7 +1,7 @@
 packages = c("dplyr", "readr", "haven", "estimatr", 
              "conleyreg", "lmtest", "sandwich", "tibble",
              "quantreg", "stringr", "spatInfer", "modelsummary",
-             "ggplot2", "tinytable", "plm", "stargazer")
+             "ggplot2", "tinytable", "plm", "stargazer", "reaxl")
 lapply(packages, library, character.only = TRUE)
 
 colony_data = read_dta("datasets/7-ajr-2001/maketable3.dta")
@@ -10,6 +10,9 @@ wb_regions = read_csv("datasets/world-regions-according-to-the-world-bank.csv") 
   janitor::clean_names() |>
   select(-entity, -year) |>
   rename(wb_region = world_region_according_to_the_world_bank)
+pwt_init = read_excel("Datasets/pwt1001.xlsx") |>
+  filter(year == 1995) |>
+  mutate(countrycode = ifelse(countrycode == "ROU", "ROM", countrycode))
 
 colony_final = colony_data |>
   left_join(colony_names) |>
@@ -18,7 +21,8 @@ colony_final = colony_data |>
     shortnam %in% c("ROM", "YUG") ~ "Europe and Central Asia",
     shortnam == "TWN" ~ "East Asia and Pacific",
     shortnam == "ZAR" ~ "Sub-Saharan Africa",
-    TRUE ~ wb_region))
+    TRUE ~ wb_region)) |>
+  left_join(pwt_init, by = c("shortnam" = "countrycode"))
 
 mod1 = lm(avexpr ~ logem4,
           data = colony_data |> filter(excolony == 1,
