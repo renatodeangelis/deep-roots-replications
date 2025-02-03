11433d05656601a251d3d10de57c95e82839fda2
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-03 16:37:22 -0600

    added function for averages

--- datasets/pwt.R
+++ datasets/pwt.R
@@ -7,4 +7,9 @@ pwt_init = read_excel("Datasets/pwt1001.xlsx")
 pwt_inter = pwt_init |>
   filter(!is.na(rgdpe))
 
-
+avg = function(x, country, start_year, end_year) {
+  x |>
+    filter(year >= start_year, year <= end_year, country == country) |>
+    pull(x) |>
+    mean(na.rm = TRUE)
+}
