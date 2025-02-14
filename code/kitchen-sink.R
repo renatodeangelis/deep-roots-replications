44d0da66acbb2152fc194f404f059f1f18909335
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-14 13:00:51 -0600

    intermediate

--- code/kitchen-sink.R
+++ code/kitchen-sink.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(ivreg)
+library(AER)
 
 pwt = read_csv("datasets/pwt_analytic.csv")
 wb_regions = read_csv("datasets/world-regions-according-to-the-world-bank.csv")
@@ -9,11 +9,43 @@ pwt_merge = pwt |>
   select(-Entity, -Year) |>
   rename(wb_region = `World Region according to the World Bank`)
   
-lm(growth ~ pop_growth + schooling + domestic_investment +
-     fertility + life_expectancy + trade_to_gdp + govt_consumption +
-     cum_inflation + `World Region according to the World Bank` + eastern +
-     hindu + jewish + muslim + orthodox + protestant + other + near_coast +
-     tropical + language + ethnic + avexpr,
-          data = pwt_merge) |>
-  summary()
+east_asia = c("CHN", "HKG", "JPN", "KOR", "MAC", "MNG")
 
+pwt_final = pwt_merge |>
+  mutate(initial_gdp = log(initial_gdp),
+         pop_growth = log(pop_growth*100 + 0.1),
+         pop_growth_instr = log((pop_growth_instr + 0.1) * 100),
+         schooling = log(schooling),
+         school_instr = log(school_instr),
+         domestic_investment = log(domestic_investment),
+         investment_instr = log(investment_instr),
+         mortality = 1/life_expectancy,
+         ssa = wb_region == "Sub-Saharan Africa" |
+           country_code == "ZAR",
+         east_asia = country_code %in% east_asia,
+         latam = wb_region == "Latin America and Caribbean")
+
+endog_vars <- c("domestic_investment", "schooling", "govt_consumption", "inflation")
+
+iv_vars <- c("ln_rgdppw_instr", "pop_growth_instr", "school_instr", "investment_instr",
+             "trade_instr", "govt_instr", "colony_esp_prt", 
+             "catholic_1900", "protestant_1900", "orthodox_1900", "muslim_1900",
+             "jewish_1900", "hindu_1900", "eastern_1900", "non-religious_1900", "other_1900",
+             "legal_origin")
+
+# Define the full model formula
+formula_iv <- as.formula("growth ~ log_income + pop_growth + fertility + mortality +
+                         trade_to_gdp + govt_consumption + inflation +
+                         protestant + orthodox + muslim + hindu + jewish + eastern + other +
+                         near_coast + tropical + language + ethnic + avexpr +
+                         ssa + latam + east_asia | . - domestic_investment - schooling - govt_consumption - inflation +
+                         ln_rgdppw_instr + pop_growth_instr + school_instr + investment_instr +
+                         trade_instr + govt_instr + colony_esp_prt +
+                         catholic_1900 + protestant_1900 + orthodox_1900 + muslim_1900 +
+                         jewish_1900 + hindu_1900 + eastern_1900 + other_1900 +
+                         legal_origin")
+
+model_iv <- ivreg(formula_iv, data = pwt_final)
+
+# View results
+summary(model_iv)
