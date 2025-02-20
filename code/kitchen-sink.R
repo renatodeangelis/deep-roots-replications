e14a48fc058bc46e538d6257c619714de4922de7
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-20 15:43:13 -0600

    getting somewhere

--- code/kitchen-sink.R
+++ code/kitchen-sink.R
@@ -1,54 +1,18 @@
-library(tidyverse)
-library(AER)
+library(readxl)
 
-pwt_final = read_csv("datasets/pwt_final.csv") |>
-  mutate(growth = growth_rate * 100)
+data = read_excel("datasets/dkt-ej-to-be-distributed.xls") |>
+  mutate(across(dum6575:ssafr, as.numeric))
 
 ## Model 1: Kitchen sink regression
 
-# First stage
-iv_initial_gdp = lm(initial_gdp ~ initial_gdp_instr, data = pwt_final)
-iv_enrollment = lm(enrollment ~ enrollment_instr, data = pwt_final)
-iv_investment = lm(ki ~ ki_instr, data = pwt_final)
-iv_pop = lm(pop_growth ~ pop_growth_instr, data = pwt_final)
-iv_trade = lm(openk ~ openk_instr, data = pwt_final)
-iv_govt = lm(kg ~ kg_instr, data = pwt_final)
-iv_inflation = lm(cum_inflation ~ colony_esp_prt, data = pwt_final)
-iv_catholic = lm(catholic ~ catholic_1900, data = pwt_final)
-iv_orthodox = lm(orthodox ~ orthodox_1900, data = pwt_final)
-iv_protestant = lm(protestant ~ protestant_1900, data = pwt_final)
-iv_hindu = lm(hindu ~ hindu_1900, data = pwt_final)
-iv_muslim = lm(muslim ~ muslim_1900, data = pwt_final)
-iv_jewish = lm(jewish ~ jewish_1900, data = pwt_final)
-iv_eastern = lm(eastern ~ eastern_1900, data = pwt_final)
-iv_other = lm(other ~ other_1900, data = pwt_final)
-iv_nonreligious = lm(`non-religious` ~ `non-religious_1900`, data = pwt_final)
-iv_formalism = lm(formalism ~ french_origin + british_origin, data = pwt_final)
-
-pwt_final$pred_initial_gdp <- predict(iv_initial_gdp, pwt_final)
-pwt_final$pred_enrollment <- predict(iv_enrollment, pwt_final)
-pwt_final$pred_investment <- predict(iv_investment, pwt_final)
-pwt_final$pred_pop <- predict(iv_pop, pwt_final)
-pwt_final$pred_trade <- predict(iv_trade, pwt_final)
-pwt_final$pred_govt <- predict(iv_govt, pwt_final)
-pwt_final$pred_inflation <- predict(iv_inflation, pwt_final)
-pwt_final$pred_catholic <- predict(iv_catholic, pwt_final)
-pwt_final$pred_orthodox <- predict(iv_orthodox, pwt_final)
-pwt_final$pred_protestant <- predict(iv_protestant, pwt_final)
-pwt_final$pred_hindu <- predict(iv_hindu, pwt_final)
-pwt_final$pred_muslim <- predict(iv_muslim, pwt_final)
-pwt_final$pred_jewish <- predict(iv_jewish, pwt_final)
-pwt_final$pred_eastern <- predict(iv_eastern, pwt_final)
-pwt_final$pred_other <- predict(iv_other, pwt_final)
-pwt_final$pred_nonreligious <- predict(iv_nonreligious, pwt_final)
-pwt_final$pred_formalism <- predict(iv_formalism, pwt_final)
-
-kitchen_sink = lm(growth_rate ~ pred_initial_gdp + pred_pop + pred_investment +
-                    pred_trade + pred_govt + period_2 + period_3,
-                  data = pwt_final)
-
-
-
-
-
+kitchen_sink = ivreg(formula = gyw ~ yw0l + lfshl + inv + gpop + opres + gv + 
+        dp + ortha + prota + hindua + muslima + jewsa + easrel2a + 
+        othrel2a + check + excondec + fertl + lifee1r + easia + ssafr + 
+        latincar + lcr100km + kgatrstr + lang + ethtens + exprsk + 
+        kkz96 + dum8595 + dum7585 | yw0llag + lfshllag + invlag + 
+        gpoplag + opreslag + gvlag + spanpor + orth1900a + prot1900a + 
+        hindu1900a + muslim1900a + jews1900a + easrel21900a + othrel21900a + 
+        frecivil + britcommon + exconlag + fertl + lifee1r + easia + 
+        ssafr + latincar + lcr100km + kgatrstr + lang + ethtens + 
+        exprsk + kkz96 + dum8595 + dum7585, data = data)
 
---
+++ datasets/dkt-replication.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(readxl)
+
+data = read_excel("datasets/dkt-ej-to-be-distributed.xls")
+
