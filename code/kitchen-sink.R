26ee05a85a202a547d63a9858931208fa2b00950
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-20 16:48:17 -0600

    done for the day

--- code/kitchen-sink.R
+++ code/kitchen-sink.R
@@ -1,18 +1,19 @@
 library(readxl)
+library(dplyr)
+library(AER)
 
 data = read_excel("datasets/dkt-ej-to-be-distributed.xls") |>
   mutate(across(dum6575:ssafr, as.numeric))
 
 ## Model 1: Kitchen sink regression
 
-kitchen_sink = ivreg(formula = gyw ~ yw0l + lfshl + inv + gpop + opres + gv + 
-        dp + ortha + prota + hindua + muslima + jewsa + easrel2a + 
-        othrel2a + check + excondec + fertl + lifee1r + easia + ssafr + 
-        latincar + lcr100km + kgatrstr + lang + ethtens + exprsk + 
-        kkz96 + dum8595 + dum7585 | yw0llag + lfshllag + invlag + 
-        gpoplag + opreslag + gvlag + spanpor + orth1900a + prot1900a + 
-        hindu1900a + muslim1900a + jews1900a + easrel21900a + othrel21900a + 
-        frecivil + britcommon + exconlag + fertl + lifee1r + easia + 
-        ssafr + latincar + lcr100km + kgatrstr + lang + ethtens + 
-        exprsk + kkz96 + dum8595 + dum7585, data = data)
-
+mod = ivreg(gyw ~ lifee1r + fertl + easia + ssafr + latincar + lcr100km +
+              kgatrstr + lang + ethtens + exprsk + kkz96 + dum7585 + dum8595 +
+              yw0l + gpop + lfshl + inv + opres + gv + dp + easrel2a + hindua +
+              jewsa + muslima + ortha + prota + othrel2a + excondec + check |
+              lifee1r + fertl + easia + ssafr + latincar + lcr100km + kgatrstr +
+              lang + ethtens + exprsk + kkz96 + dum7585 + dum8595 + yw0llag +
+              gpoplag + lfshllag + invlag + opreslag + gvlag + spanpor +
+              easrel21900a + hindu1900a + jews1900a + muslim1900a + orth1900a +
+              prot1900a + othrel21900a + exconlag + frecivil + britcommon,
+            data = data)
