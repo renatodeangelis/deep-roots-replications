4572c73934dd4fc3c87befb9ffe32ab5a3f3436e
Renato de Angelis <renatodeangelis12@gmail.com>
2025-02-27 14:47:16 -0600

    completed table 1

--- code/9-guinnane-hoffman/guinnane-hoffman-script.R
+++ code/9-guinnane-hoffman/guinnane-hoffman-script.R
@@ -4,6 +4,8 @@ sapply(packages, library, character.only = TRUE)
 
 nazi_data = read_dta("datasets/5-voigtlander-voth/Dataset_QJE_Replicate_with_Cities(1).dta")
 
+## Generating variables
+
 nazi_final = nazi_data |>
   filter(judaica == 1 | comm1349 == 1) |>
   mutate(nazi_1928 = n285nsda / n285gs,
@@ -23,27 +25,134 @@ nazi_final = nazi_data |>
            TRUE ~ frac_jew_33),
          prop_deport = 100 * deptotal / jews33,
          syn_attack = syndam == 1 | syndest == 1,
-#         syn_33 = case_when(
-#           syn33 == 0 & betraum == 1 ~ 1,
-#           TRUE ~ syn_33)
-         )
+         syn33 = case_when(
+           syn33 == 0 & betraum == 1 ~ 1,
+           TRUE ~ syn33))
 
-## Guinnane & Hoffman Table 1
-
-kreis_nr = nazi_final |> 
-  pull(kreis_nr)
+## Principal components
 
 nazi_final = nazi_final |>
   mutate(std_pog20s = pog20s / pop33,
-         std_sturmer = sturmer / pop33,
+         std_stuermer = sturmer / pop33,
          std_deport = prop_deport / pop33,
          std_syn_attack = syn_attack / pop33)
 
-pca1 = nazi_final |>
+pca1 <- nazi_final |>
   filter(syn33 == 1) |>
-  select(std_pog20s, nazi_1928, dvfp_1924, std_deport, std_sturmer, std_syn_attack) |>
-  prcomp() |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_deport, std_syn_attack) |>
+  na.omit() |>
+  prcomp(center = TRUE, scale. = TRUE)
+
+pca1_scores <- predict(pca1)[, 1]
+
+pca1_data <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_deport, std_syn_attack) |>
+  na.omit()
+
+nazi_final <- nazi_final |>
+  mutate(row_id = row_number())
+
+pca1_rows <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_deport, std_syn_attack, row_id) |>
+  na.omit() |>
+  pull(row_id)
+
+nazi_final <- nazi_final |>
+  mutate(PCA_20C_AS = NA_real_)
+
+for(i in seq_along(pca1_rows)) {
+  nazi_final$PCA_20C_AS[pca1_rows[i]] <- pca1_scores[i]
+}
+
+pca2 <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_syn_attack) |>
+  na.omit() |>
+  prcomp(center = TRUE, scale. = TRUE)
+
+pca2_scores <- predict(pca2)[, 1]
+
+pca2_rows <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_syn_attack, row_id) |>
+  na.omit() |>
+  pull(row_id)
+
+for(i in seq_along(pca2_rows)) {
+  if(is.na(nazi_final$PCA_20C_AS[pca2_rows[i]])) {
+    nazi_final$PCA_20C_AS[pca2_rows[i]] <- pca2_scores[i]
+  }
+}
+
+pca3 <- nazi_final |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_deport) |>
+  na.omit() |>
+  prcomp(center = TRUE, scale. = TRUE)
+
+pca3_scores <- predict(pca3)[, 1]
+
+pca3_rows <- nazi_final |>
+  select(std_pog20s, dvfp_1924, nazi_1928, std_stuermer, std_deport, row_id) |>
+  na.omit() |>
+  pull(row_id)
+
+for(i in seq_along(pca3_rows)) {
+  if(is.na(nazi_final$PCA_20C_AS[pca3_rows[i]])) {
+    nazi_final$PCA_20C_AS[pca3_rows[i]] <- pca3_scores[i]
+  }
+}
+
+pca4 <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(dvfp_1924, nazi_1928, std_stuermer, std_deport, std_syn_attack) |>
+  na.omit() |>
+  prcomp(center = TRUE, scale. = TRUE)
+
+pca4_scores <- predict(pca4)[, 1]
+
+pca4_rows <- nazi_final |>
+  filter(syn33 == 1) |>
+  select(dvfp_1924, nazi_1928, std_stuermer, std_deport, std_syn_attack, row_id) |>
+  na.omit() |>
+  pull(row_id)
+
+for(i in seq_along(pca4_rows)) {
+  if(is.na(nazi_final$PCA_20C_AS[pca4_rows[i]]) && nazi_final$syn33[pca4_rows[i]] == 1) {
+    nazi_final$PCA_20C_AS[pca4_rows[i]] <- pca4_scores[i]
+  }
+}
+
+nazi_final <- nazi_final |>
+  select(-row_id)
+
+vars_to_std <- c("PCA_20C_AS", "pog1349", "ln_pop_28", "ln_pop_33", 
+                 "frac_jew_25", "frac_jew_33", "frac_prot_25")
+
+# Apply standardization to each variable
+for(var in vars_to_std) {
+  var_data <- nazi_final[[var]][!is.na(nazi_final$pog1349)]
+  var_mean <- mean(var_data, na.rm = TRUE)
+  var_sd <- sd(var_data, na.rm = TRUE)
+  
+  nazi_final[[paste0("Std_", var)]] <- NA_real_
+  nazi_final[[paste0("Std_", var)]][!is.na(nazi_final$pog1349)] <- 
+    (var_data - var_mean) / var_sd
+}
+
+# Display summary of standardized variables
+std_vars <- paste0("Std_", vars_to_std)
+summary_data <- nazi_final |> 
+  select(all_of(std_vars)) |> 
   summary()
+
+print(summary_data)
+
+## Table 1
+
+kreis_nr = nazi_final |> 
+  pull(kreis_nr)
 
 mod1 = lm(nazi_1928 ~ pog1349 + ln_pop_28 + frac_jew_25 + frac_prot_25,
           data = nazi_final)
@@ -51,9 +160,15 @@ mod1_res = coeftest(mod1, vcov = vcovCL(mod1, cluster = kreis_nr))
 
 mod2 = rq(nazi_1928 ~ pog1349 + ln_pop_28 + frac_jew_25 + frac_prot_25,
           data = nazi_final)
-summary(mod2)
 mod2_res = summary(mod2, se = "boot", cluster = kreis_nr, R = 1000)
 
+mod3 = lm(Std_PCA_20C_AS ~ pog1349 + Std_ln_pop_33 + Std_frac_jew_33 + Std_frac_prot_25,
+          data = nazi_final)
+mod3_res = coeftest(mod3, vcov = vcovCL(mod3, cluster = kreis_nr))
+
+mod4 = rq(Std_PCA_20C_AS ~ pog1349 + Std_ln_pop_33 + Std_frac_jew_33 + Std_frac_prot_25,
+          data = nazi_final)
+mod4_res = summary(mod4, se = "boot", cluster = kreis_nr, R = 1000)
 
 mod5 = glm(deptotal ~ pog1349 + ln_pop_33 + ln_jews_33 + frac_jew_33 + frac_prot_25,
            data = nazi_final,
@@ -64,14 +179,6 @@ mod6 = glm(deptotal ~ pog1349 + ln_pop_33 + frac_jew_33 + frac_prot_25,
            data = nazi_final |> filter(!is.na(ln_jews_33)),
            family = poisson)
 mod6_res = coeftest(mod6, vcov = vcovCL(mod6, type = "HC0"))
-
-## Table 2
-
-#reg pcDVFP245 pog1349 logpop245 perc_JEW25 perc_PROT25
-
-mod7 = lm(dvfp_1924 ~ pog1349 + ln_pop_24 + frac_jew_25 + frac_prot_25,
-          data = nazi_final)
-mod7_res = coeftest(mod7, vcov = vcovCL(mod7, cluster = kreis_nr))
 
 ## Table 3
 
